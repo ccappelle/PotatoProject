c9977d626bae777513a08d667cadcad3ecd7a830
Collin Cappelle <collin.cappelle@gmail.com>
2016-10-25 20:14:35 -0400

    mutating potatoes

--- networks.py
+++ networks.py
@@ -2,7 +2,7 @@ import numpy as np
 
 class LayeredNetwork():
 
-	def __init__(self,num_sensors=4,num_layers=1, num_hidden=8, num_motors=8,back_connections=0,hidden_recurrence=0,motor_recurrence=0):
+	def __init__(self,num_sensors=4,num_layers=1, num_hidden=8, num_motors=1,back_connections=0,hidden_recurrence=0,motor_recurrence=0):
 		#Create an layered network with random weights returned as adjacency matrix
 		#Dimensions and structure: num_sensors -> num_hidden_{1} -> ... -> num_hidden_{num_layers} -> num_motors
 		#can add feed back connections between hidden neurons (back_connections=1)
@@ -52,14 +52,54 @@ class LayeredNetwork():
 			for neuron_ID in range(num_sensors+num_layers*num_hidden, total_neurons):
 				self.adj_matrix[neuron_ID,neuron_ID] = rand_mat[neuron_ID,neuron_ID]
 
+		self.total_synapses = np.count_nonzero(self.adj_matrix)
 
 	def Get_Adj_Matrix():
 		return self.adj_matrix
 
+	def Mutate_One_Synapse(self,fromID,toID,sigma=-1):
+		if sigma<=0:
+			sigma = self.adj_matrix[fromID,toID]
+			if sigma<0.1:
+				sigma = 0.1
 
+		if not(self.adj_matrix[fromID,toID] == 0):
+			self.adj_matrix[fromID,toID] = self.adj_matrix[fromID,toID] + np.random.normal(0,sigma)
+
+	def Mutate_p(self,p=-1,sigma=-1):
+		if p<0:
+			p= 1./float(self.total_synapses)
+
+		synapse_Indices = np.nonzero(self.adj_matrix)
+		rands = np.random.rand(self.total_synapses)
+
+		for i in range(self.total_synapses):
+			if rands[i]<p:
+				fromID,toID = synapse_Indices[0][i],synapse_Indices[1][i]
+				self.Mutate_One_Synapse(fromID,toID,sigma)
+
+	def Mutate_n(self,n,sigma=-1):
+		synapse_Indices = np.nonzero(self.adj_matrix)
+		rands = np.random.permutation(self.total_synapses)
+
+		for i in range(self.total_synapses):
+			fromID, toID = synapse_Indices[0][rands[i]], synapse_Indices[1][rands[i]]
+			self.Mutate_One_Synapse(fromID,toID,sigma)
+
+def Create_Quad_Network(num_layers=1,num_hidden=8,back_connections=0,hidden_recurrence=0,motor_recurrence=0):
+	QUAD_SENSORS = 4
+	QUAD_MOTORS = 8
+	return LayeredNetwork(num_sensors=QUAD_SENSORS,num_layers=num_layers,num_hidden=num_hidden,num_motors=QUAD_MOTORS,
+							back_connections=back_connections,hidden_recurrence=hidden_recurrence,
+							motor_recurrence=motor_recurrence)
+
+def Create_Biped_Network(num_layers=1,num_hidden=8,back_connections=0,hidden_recurrence=0,motor_recurrence=0):
+	BIPED_SENSORS = 2
+	BIPED_MOTORS = 6
+	return LayeredNetwork(num_sensors=QUAD_SENSORS,num_layers=num_layers,num_hidden=num_hidden,num_motors=QUAD_MOTORS,
+							back_connections=back_connections,hidden_recurrence=hidden_recurrence,
+							motor_recurrence=motor_recurrence)
 
 if __name__ == "__main__":
-	myNet = LayeredNetwork()
-	print isinstance(myNet,LayeredNetwork)
-	print isinstance(myNet.adj_matrix,LayeredNetwork)
-	print myNet.adj_matrix
+	myNet = Create_Quad_Network()
+	myNet.Mutate_p()
