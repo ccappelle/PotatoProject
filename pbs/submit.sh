08b89f6d149ab264b478c98e65f5e9cbf21ff8e8
Collin Cappelle <collin.cappelle@gmail.com>
2017-01-19 18:49:06 -0500

    sync with vacc 17_019_18_49_06

--- pbs/submit.sh
+++ pbs/submit.sh
@@ -1,30 +1,30 @@
 #!/bin/bash
 if [ -z "$1" ]
 then
-	echo "Num trials not specified"
+	echo "Num not specified"
 	exit 1
 fi
 echo "Num trials: " $1
 
 if [ -z "$2" ]
 then
-	echo "Dev Layers not specified"
+	echo "Robo Type not specified"
 	exit 1
 fi
-echo "Dev Layers: " $2
+echo "Robotype: " $2
 
 
 if [ -z "$3" ]
 then
-	echo "Generations not specified"
+	echo "Dimension not specified"
 	exit 1
 fi
-echo "Generations: " $3
+echo "Dimensions: " $3
 
 for x in `seq 1 $1`
 do
 	#mkdir -p ~/scratch/logs/$x/$y
-	qsub -vARG_TRIAL=$x,ARG_DEVO=$2,ARG_GENS=$3 ~/runners/single_runner.pbs 
-	echo "Run $x started with devo $2"
+	qsub -vARG_TRIAL=$x,ARG_TYPE=$2,ARG_DIM=$3 ~/runners/single_runner.pbs 
+	echo "Run $x started with type $2"
 	sleep 1;
 done
